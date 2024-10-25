472ce5cc3f73be8fc21cdf490368ca9eb9eda862
Stephen Lee <leestephen0320@gmail.com>
2024-10-25 14:10:58 -0700

    finished models/index.ts

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,6 +1,12 @@
 import sequelize from '../config/connection.js';
+import { LeaderboardFactory } from './leaderboard.js';
+import { PuzzleFactory } from './puzzles.js';
+import { RoomsFactory } from './rooms.js';
 import { UserFactory } from './user.js';
 
+const Leaderboard = LeaderboardFactory(sequelize);
+const Puzzle = PuzzleFactory(sequelize);
+const Room = RoomsFactory(sequelize);
 const User = UserFactory(sequelize);
 
-export { User };
+export { Leaderboard, Puzzle, Room, User };
