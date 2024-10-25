a1bafb0a2d3979424498144fe72326d1fa189316
Stephen Lee <leestephen0320@gmail.com>
2024-10-25 12:22:44 -0700

    the first user seed

--- server/src/seeds/user-seeds.ts
+++ server/src/seeds/user-seeds.ts
@@ -3,16 +3,10 @@ import { User } from '../models/index.js';
 export const seedUsers = async () => {
   await User.bulkCreate(
     [
-      { username: 'JollyGuru', email: 'jolly@guru.com', password: 'password' },
-      {
-        username: 'SunnyScribe',
-        email: 'sunny@scribe.com',
+      { username: 'JollyGuru', 
+        email: 'jolly@guru.com', 
         password: 'password',
-      },
-      {
-        username: 'RadiantComet',
-        email: 'radiant@comet.com',
-        password: 'password',
+        progress: 1,
       },
     ],
     { individualHooks: true }
