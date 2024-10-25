6e63b0c766fb9e085f29c14d185102a406794283
Stephen Lee <leestephen0320@gmail.com>
2024-10-25 10:22:39 -0700

    basic folder structure

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import { Outlet } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <main className='container pt-5'>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
+
+export default App;
