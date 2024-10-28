987036d96619e5ed52bf2424ebbe396f971da921
Stephen Lee <leestephen0320@gmail.com>
2024-10-28 14:05:33 -0700

    PUZZles seeded

--- server/src/seeds/index.ts
+++ server/src/seeds/index.ts
@@ -1,5 +1,7 @@
 import { seedUsers } from './user-seeds.js';
 import { seedLeaderboard } from './leaderboard-seed.js';
+import { seedRooms } from './room-seeds.js';
+import { seedPuzzles } from './puzzle-seeds.js';
 import sequelize from '../config/connection.js';
 
 const seedAll = async (): Promise<void> => {
@@ -11,7 +13,13 @@ const seedAll = async (): Promise<void> => {
     console.log('\n----- USERS SEEDED -----\n');
 
     await seedLeaderboard();
-    console.log('\n----- USERS SEEDED -----\n');
+    console.log('\n----- LEADERBAORD SEEDED -----\n');
+
+    await seedPuzzles();
+    console.log('\n----- PUZZLES SEEDED -----\n');
+
+    await seedRooms();
+    console.log('\n----- ROOMS SEEDED -----\n');
 
     process.exit(0);
   } catch (error) {
